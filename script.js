ad27c4815f94040f571a75aa77d4d4619fbbdaa6
Rudrarapu Haarika <haarikarudrarapu@gmail.com>
2026-09-23 17:20:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius = document.getElementById("radius").value; let volume = document.getElementById("volume"); radius = Number(radius); if (radius < 0 || isNaN(radius)) { volume.value = "NaN"; return false; } let result = (4 / 3) * Math.PI * radius * radius * radius; volume.value = result.toFixed(4); return false; } document.getElementById("MyForm").addEventListener("submit", function(event) { event.preventDefault(); volume_sphere();
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
